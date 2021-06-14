2e01f70ce31db705bc4a31ff5c0e160b671206f5
Vehbi Erbil Karakuş <80065498+demirkarakus@users.noreply.github.com>
2021-06-14 14:06:31 +0300

    New lessons

---
+++ ReCapDemo/src/DortIslem.java
@@ -0,0 +1,14 @@
+
+public class DortIslem {
+
+		public int Topla(int sayi1, int sayi2){
+			return sayi1 + sayi2;
+		}
+		
+		public int cikart(int sayi1, int sayi2) {
+			return sayi1 - sayi2;
+		}
+		
+		
+		
+}
