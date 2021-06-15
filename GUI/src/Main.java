d0c3125e7819678b4ba7d583c5f3c0569c741692
Vehbi Erbil Karakuş <80065498+demirkarakus@users.noreply.github.com>
2021-06-15 12:04:24 +0300

    Update Main.java

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -1,5 +1,8 @@
 //Java Dersi #98
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -7,19 +10,27 @@ import javax.swing.JPanel;
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setSize(200,200);
-		
-		JPanel panel = new JPanel();
+		JFrame frame = new JFrame("Button Örneği");
+
 		
 		//Button ekleme
-		JButton btn = new JButton("Button");
-		panel.add(btn);
+		JButton btn = new JButton("Gönder");
+		
+		btn.setBounds(100, 100, 100, 30);
+		frame.add(btn);
+		
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Butona basıldı.");
+			}
+		});
+		
 		
 		//
-		
-		
-		frame.add(panel);
+		frame.setSize(400,400);
+		frame.setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
